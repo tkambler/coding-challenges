b13f16e7ccb335a76cac27256cc40d0ea72db00f
Tim Ambler <timothy.ambler@cigna.com>
2023-06-07 10:23:29 -0500

    misc

---
+++ scripts/encrypt.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+const MiniZip = require('minizip-asm.js');
+const { sync: glob } = require('glob');
+
+(async () => {
+
+  const { PASSWORD } = process.env;
+  if (!PASSWORD) {
+    throw new Error(`The PASSWORD environment variable must be set.`);
+  }
+
+  const mz = new MiniZip();
+
+  const solutions = glob('**/solution.js', {
+    cwd: path.resolve(__dirname, '..'),
+  });
+
+  for (const solution of solutions) {
+    const content = fs.readFileSync(solution, 'utf-8');
+    mz.append(solution, content, {
+      password: PASSWORD,
+    });
+  }
+
+  fs.writeFileSync('solutions.zip', Buffer.from(mz.zip()));
+
+  console.log('Done.');
+
+})();
